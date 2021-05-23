71dbdcd3e95c62a776ad05e5b7dcf14fb9592b0a
Emil Lewandowski <emilemil.emil@gmail.com>
2021-05-23 17:07:35 +0200

    init challenge 3

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import "./App.css";
 import Header from "./components/Header/Header";
 // import MainSection from "./components/MainSection/MainSection";
 // import Challenge1 from "./components/Challenge1/Challenge1";
-import Challenge2 from "./components/Challenge2/Challenge2";
+// import Challenge2 from "./components/Challenge2/Challenge2";
+import Challenge3 from "./components/Challenge3/Challenge3";
 
 function App() {
   return (
     <div className="App">
       {/* <Header /> */}
-      <Challenge2 />
+      <Challenge3 />
     </div>
   );
 }
---
+++ src/components/Challenge3/Challenge3.js
@@ -0,0 +1,20 @@
+import React from "react";
+import TitleSection from "./TitleSection";
+import CardSection from "./CardSection";
+
+class Challenge3 extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <TitleSection />
+        <CardSection />
+      </div>
+    );
+  }
+}
+
+export default Challenge3;
